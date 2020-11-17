e3619dadcd4e05a4a3050323737be48e0581be24
liwenkang <changzhiliwenkang@outlook.com>
2020-11-17 11:05:08 +0800

    delete .idea dir

---
+++ 前端开发核心知识进阶/foo.js
@@ -0,0 +1,14 @@
+function Foo() {
+    this.bar = 'Lucas';
+}
+
+const instance = new Foo();
+console.log(instance.bar);
+
+function MyNew() {
+    const obj = Object.create(null);
+
+    obj.__proto__ = 
+}
+
+MyNew();
